bfa2477d50b02a7f70cf687aabc33f7c93c3d160
Elliot Pettingale <elliot@kalexiko.com>
2018-10-09 09:50:46 +0100

    implement transform methods

--- src/Services/Transform.php
+++ src/Services/Transform.php
@@ -4,10 +4,11 @@ namespace EliPett\EntityTransformer\Services;
 
 use EliPett\EntityTransformer\Contracts\EntityTransformer;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 
 class Transform
 {
-    public static function entities($entities, EntityTransformer $transformer): array
+    public static function entities($entities, string $transformer): array
     {
         $items = [];
 
@@ -18,8 +19,46 @@ class Transform
         return [];
     }
 
-    public static function entity(Model $entity, EntityTransformer $transformer): array
+    public static function entity(Model $entity, string $entityTransformerPath): array
     {
+        $entityTransformer = self::getTransformerInstance($entityTransformerPath);
+
+        $data = [];
+
+        foreach ($entityTransformer->data($entity) as $key => $value) {
+            $data[$key] = $value;
+        }
+
+        foreach ($entityTransformer->relations() as $key => $relationTransformerPath) {
+            if ($entity->relationLoaded($key)) {
+                $data[$key] = self::relation($entity->$key, $relationTransformerPath);
+            }
+        }
+
         return [];
+    }
+
+    private static function relation($relation, string $transformer): array
+    {
+        if ($relation instanceof Collection) {
+            return self::entities($relation, $transformer);
+        }
+
+        if ($relation instanceof Model) {
+            return self::entity($relation, $transformer);
+        }
+
+        throw new \InvalidArgumentException('Invalid Relation: ' . \get_class($relation));
+    }
+
+    public static function getTransformerInstance(string $class): EntityTransformer
+    {
+        $transformer = new $class();
+
+        if (!$transformer instanceof EntityTransformer) {
+            throw new \InvalidArgumentException("Invalid Transformer: $class");
+        }
+
+        return $transformer;
     }
 }
