c3469d48a1c02b752c278f571953d0de919e3c70
Elliot Pettingale <elipettingale@gmail.com>
2019-04-11 10:40:35 +0100

    refactor static class as non-static with facade

---
+++ src/Facades/Transform.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace EliPett\EntityTransformer\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Transform extends Facade
+{
+    protected static function getFacadeAccessor() { return 'transform'; }
+}
